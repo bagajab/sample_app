6afaf06a664458ea9158067a7e7fdcbe4fe993d0
Bagaja Birhanu <bagajab@gmail.com>
2023-02-28 14:24:21 +0300

    Finish user signup

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,34 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  test 'Invalid signup information' do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, params: {
+        user: {
+          name: '',
+          email: 'user@invalid',
+          password: 'foo',
+          password_confirmation: 'bar'
+        }
+      }
+    end
+    assert_response :unprocessable_entity
+    assert_template 'users/new'
+  end
+
+  test 'Valid signup information' do
+    assert_difference 'User.count', 1 do
+      post users_path, params: {
+        user: {
+          name: 'Bagaja Guracha Nura',
+          email: 'bagajab@gmail.com',
+          password: 'randompassword',
+          password_confirmation: 'randompassword'
+        }
+      }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+  end
+end
