0d3a1c021e07783f5d5733f47882d7bcf8918a53
Bagaja Birhanu <bagajab@gmail.com>
2023-02-28 17:06:37 +0300

    Added login and installed importmaps

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,16 @@
+class SessionsController < ApplicationController
+  def create
+    user = User.find_by_email(params[:session][:email].downcase)
+    if user&.authenticate(params[:session][:password])
+      reset_session
+      log_in user
+      redirect_to user
+    else
+      # Create an error message.
+      flash.now[:danger] = 'Invalid email/password combination.'
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  def new; end
+end
