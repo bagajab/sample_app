1be48a7368cc6ec0144797de320524347f1d8429
Bagaja Birhanu <bagajab@gmail.com>
2023-03-06 05:42:03 +0300

    User authorization

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[show edit update]
+  before_action :logged_in_user, only: %i[show edit update]
+  before_action :correct_user, only: %i[show edit update]
 
   def create
     @user = User.new(user_params)
@@ -18,7 +20,7 @@ class UsersController < ApplicationController
 
   def update
     if @user.update(user_params)
-      flash[:success] = 'Profile updated'
+      flash[:success] = 'Profile updated.'
       redirect_to @user
     else
       render 'edit', status: :unprocessable_entity
@@ -39,5 +41,21 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:email, :name, :password, :password_confirmation)
+  end
+
+  # before filter
+
+  # confirms logged-in user.
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = 'Please login.'
+      redirect_to login_url, status: :see_other
+    end
+  end
+
+  # confirm correct user
+  def correct_user
+    redirect_to(root_url, status: :see_other) unless current_user?(@user)
   end
 end
